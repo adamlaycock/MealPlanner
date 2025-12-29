a3994aa870d43d3313d5e7bd9b568a159245d1c9
adamlaycock <adam.m.laycock@gmail.com>
2025-12-29 18:05:40 +0000

    remove old add_recipe

--- main_script.py
+++ main_script.py
@@ -21,25 +21,6 @@ def build_list(ids: list) -> pd.DataFrame:
 
     return(ingreds)
 
-def add_recipe(name: str, link: str, ingreds: list):
-    global RECIPE_DF, INGREDS_DF
-
-    ingreds = [ing.lower() for ing in ingreds]
-
-    new_id = RECIPE_DF['ID'].max() + 1
-    RECIPE_DF.loc[len(RECIPE_DF)] = [name, link, new_id]
-
-    new_rows = pd.DataFrame({
-        'Ingredient': ingreds,
-        'ID': new_id
-    })
-    INGREDS_DF = pd.concat([INGREDS_DF, new_rows])
-
-    # INGREDS_DF.to_csv(
-    #     r'C:\Users\adamm\OneDrive\Documents\R&D\MealPlanner\ingredients.csv',
-    #     index=False
-    # )
-
 def remove_recipe(name:str):
     global RECIPE_DF, INGREDS_DF
 
