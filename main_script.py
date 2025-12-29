c6b4392d4770771350d1d629e15623e857958a8f
adamlaycock <adam.m.laycock@gmail.com>
2025-12-29 10:12:23 +0000

    add recipe file and add_recipe function

--- main_script.py
+++ main_script.py
@@ -4,6 +4,10 @@ INGREDS_DF = pd.read_csv(
     r'C:\Users\adamm\OneDrive\Documents\R&D\MealPlanner\ingredients.csv'
 )
 
+RECIPE_DF = pd.read_csv(
+    r'C:\Users\adamm\OneDrive\Documents\R&D\MealPlanner\recipes.csv'
+)
+
 def build_list(ids: list) -> pd.DataFrame:
     filt_ingreds_df = INGREDS_DF[INGREDS_DF['ID'].isin(ids)]['Ingredient']
     ingreds = pd.DataFrame(
@@ -15,5 +19,21 @@ def build_list(ids: list) -> pd.DataFrame:
 
     return(ingreds)
 
-df = build_list([1, 9, 10])
-df
+def add_recipe(name: str, link: str, ingreds: list):
+    global RECIPE_DF, INGREDS_DF
+
+    ingreds = [ing.lower() for ing in ingreds]
+
+    new_id = RECIPE_DF['ID'].max() + 1
+    RECIPE_DF.loc[len(RECIPE_DF)] = [name, link, new_id]
+
+    new_rows = pd.DataFrame({
+        'Ingredient': ingreds,
+        'ID': new_id
+    })
+    INGREDS_DF = pd.concat([INGREDS_DF, new_rows])
+
+    # INGREDS_DF.to_csv(
+    #     r'C:\Users\adamm\OneDrive\Documents\R&D\MealPlanner\ingredients.csv',
+    #     index=False
+    # )
